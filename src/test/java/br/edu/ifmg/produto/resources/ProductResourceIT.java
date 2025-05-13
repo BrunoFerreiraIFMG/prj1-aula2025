5e7bb366fb40fa558450b04d363cc59b7a70c304
Bruno Ferreira <bruno.ferreira@ifmg.edu.br>
2025-05-13 15:41:30 -0300

    aula 12 - final dos testes

--- src/test/java/br/edu/ifmg/produto/resources/ProductResourceIT.java
+++ src/test/java/br/edu/ifmg/produto/resources/ProductResourceIT.java
@@ -1,7 +1,9 @@
 package br.edu.ifmg.produto.resources;
 
+import br.edu.ifmg.produto.dtos.ProductDTO;
 import br.edu.ifmg.produto.util.Factory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,6 +64,123 @@ public class ProductResourceIT {
                         .value("PC Gamer"));
 
     }
+
+    @Test
+    public void updateShouldReturnDtoWhenIdExists() throws Exception {
+
+        ProductDTO dto = Factory.createProductDTO();
+        String dtoJson = objectMapper.writeValueAsString(dto);
+        System.out.println(dtoJson);
+        String nameExpected = dto.getName();
+        String descriptionExpected = dto.getDescription();
+
+        ResultActions result =
+                mockMvc.perform(
+                        put("/product/{id}", existingId)
+                                .content(dtoJson)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingId));
+        result.andExpect(jsonPath("$.name").value(nameExpected));
+        result.andExpect(jsonPath("$.description").value(descriptionExpected));
+
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+
+        ProductDTO dto = Factory.createProductDTO();
+        String dtoJson = objectMapper.writeValueAsString(dto);
+
+        ResultActions result =
+                mockMvc.perform(
+                        put("/product/{id}", nonExistingId)
+                                .content(dtoJson)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+        result.andExpect(status().isNotFound());
+
+    }
+
+
+    @Test
+    public void insertShouldReturnNewObjectWhenDataAreCorrect() throws Exception {
+
+        ProductDTO dto = Factory.createProductDTO();
+        String dtoJson = objectMapper.writeValueAsString(dto);
+
+        String nameExpected = dto.getName();
+        Long idExpected = 26L;
+
+        ResultActions result =
+                mockMvc.perform(
+                        post("/product")
+                                .content(dtoJson)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+
+
+        result.andExpect(status().isCreated());
+        result.andExpect(jsonPath("$.id").value(idExpected));
+        result.andExpect(jsonPath("$.name").value(nameExpected));
+
+
+
+    }
+
+    @Test
+    public void deleteShouldReturnNoContentWhenIdExists() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(
+                        delete("/product/{id}", existingId)
+                );
+        result.andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void deleteShouldReturnNoFoundWhenIdDoesNotExists() throws Exception {
+
+
+        ResultActions result =
+                mockMvc.perform(
+                        delete("/product/{id}", nonExistingId)
+                );
+        result.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void findByIdShouldReturnProductWhenIdExists() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(
+                        get("/product/{id}", existingId)
+                                .accept(MediaType.APPLICATION_JSON)
+                );
+
+        result.andExpect(status().isOk());
+        String resultJson =
+           result.andReturn().getResponse().getContentAsString();
+        System.out.println(resultJson);
+
+        ProductDTO dto =
+             objectMapper.readValue(resultJson, ProductDTO.class);
+
+        Assertions.assertEquals(existingId, dto.getId());
+        Assertions.assertEquals("The Lord of the Rings", dto.getName());
+    }
+
+
+
+
+
+
 
 
 }
