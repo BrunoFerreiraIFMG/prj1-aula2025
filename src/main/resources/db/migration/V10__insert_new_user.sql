fa7698566e6df4423501acec794bd3faecc9d3fc
Bruno Ferreira <bruno.ferreira@ifmg.edu.br>
2025-06-12 18:00:01 -0300

    aula 21 - recuperação de senha

---
+++ src/main/resources/db/migration/V10__insert_new_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Bruno', 'Ferreira', 'bruno.ferreira@ifmg.edu.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+
